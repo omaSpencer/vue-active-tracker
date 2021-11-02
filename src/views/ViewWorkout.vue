a150f6fef03ad1e812a41eb3b8d1436ce03bf7a6
Zoltan Busi <oma.spencer@gmail.com>
2021-11-02 13:11:31 +0100

    push to prod

---
+++ src/views/ViewWorkout.vue
@@ -0,0 +1,399 @@
+<template>
+	<div class="max-w-screen-sm mx-auto px-4 py-10">
+		<!-- App Message -->
+		<div v-if="statusMsg || errorMsg" class="mb-10 p-4 rounded-md shadow-md bg-light-grey">
+			<p class="text-at-light-green">{{ statusMsg }}</p>
+			<p class="text-red-500">{{ errorMsg }}</p>
+		</div>
+
+		<div v-if="dataLoaded">
+			<!-- General Workout Info -->
+			<div class="flex flex-col items-center p-8 rounded-md shadow-md bg-light-grey relative">
+				<div v-if="user" class="absolute flex left-2 top-2 gap-x-2">
+					<span
+						@click="editMode"
+						class="
+							h-7
+							w-7
+							rounded-full
+							flex
+							justify-center
+							items-center
+							cursor-pointer
+							bg-at-light-green
+							shadow-lg
+						"
+					>
+						<img class="h-3.5 w-auto" src="@/assets/images/pencil-light.png" alt="edit icon" />
+					</span>
+					<span
+						class="
+							h-7
+							w-7
+							rounded-full
+							flex
+							justify-center
+							items-center
+							cursor-pointer
+							bg-at-light-green
+							shadow-lg
+						"
+					>
+						<img
+							@click="deleteWorkout"
+							class="h-3.5 w-auto"
+							src="@/assets/images/trash-light.png"
+							alt="trash icon"
+						/>
+					</span>
+				</div>
+				<img
+					v-if="data.workoutType === 'cardio'"
+					class="h-24 auto"
+					src="@/assets/images/running-light-green.png"
+					alt="cardio img"
+				/>
+				<img v-else class="h-24 auto" src="@/assets/images/dumbbell-light-green.png" alt="strength img" />
+				<span class="mt-6 py-1.5 px-5 text-xs text-white bg-at-light-green rounded-lg shadow-md">{{
+					data.workoutType
+				}}</span>
+				<div class="w-full mt-6">
+					<input
+						v-if="edit"
+						v-model="data.workoutName"
+						type="text"
+						class="p-2 w-full text-gray-500 focus:outline-none"
+					/>
+					<h1 v-else class="text-at-light-green text-2xl text-center">{{ data.workoutName }}</h1>
+				</div>
+			</div>
+			<!-- Exercises -->
+			<div class="mt-10 p-8 rounded-md flex flex-col items-center bg-light-grey shadow-md">
+				<!-- Strength Training -->
+				<div v-if="data.workoutType === 'strength'" class="flex flex-col gap-y-4 w-full">
+					<div
+						v-for="(item, idx) in data.exercises"
+						:key="idx"
+						class="flex flex-col gap-x-6 gap-y-2 relative sm:flex-row"
+					>
+						<div class="flex flex-2 flex-col md:w-1/3">
+							<label for="exercise-name" class="mb-1 text-sm text-at-light-green">Exercise</label>
+							<input
+								v-if="edit"
+								v-model="item.exercise"
+								id="exercise-name"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							/>
+							<p v-else>{{ item.exercise }}</p>
+						</div>
+						<div class="flex flex-1 flex-col">
+							<label for="sets" class="mb-1 text-sm text-at-light-green">Sets</label>
+							<input
+								v-if="edit"
+								v-model="item.sets"
+								id="sets"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							/>
+							<p v-else>{{ item.sets }}</p>
+						</div>
+						<div class="flex flex-1 flex-col">
+							<label for="reps" class="mb-1 text-sm text-at-light-green">Reps</label>
+							<input
+								v-if="edit"
+								v-model="item.reps"
+								id="reps"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							/>
+							<p v-else>{{ item.reps }}</p>
+						</div>
+						<div class="flex flex-1 flex-col">
+							<label for="weight" class="mb-1 text-sm text-at-light-green">Weight (KG)</label>
+							<input
+								v-if="edit"
+								v-model="item.weight"
+								id="weight"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							/>
+							<p v-else>{{ item.weight }}</p>
+						</div>
+						<img
+							v-if="edit"
+							@click="deleteExercise(item.id)"
+							class="absolute h-4 w-auto -left-5 cursor-pointer"
+							src="@/assets/images/trash-light-green.png"
+							alt="trash icon"
+						/>
+					</div>
+					<button
+						v-if="edit"
+						@click="addExercise"
+						type="button"
+						class="
+							mt-6
+							py-2
+							px-6
+							rounded-sm
+							self-start
+							text-sm text-white
+							bg-at-light-green
+							duration-200
+							border-solid border-2 border-transparent
+							hover:border-at-light-green hover:bg-white hover:text-at-light-green
+						"
+					>
+						Add Exercise
+					</button>
+				</div>
+				<!-- Cardio -->
+				<div v-else class="flex flex-col gap-y-4 w-full">
+					<div
+						v-for="(item, idx) in data.exercises"
+						:key="idx"
+						class="flex flex-col gap-x-6 gap-y-2 relative sm:flex-row"
+					>
+						<div class="flex flex-2 flex-col md:w-1/3">
+							<label for="cardioType" class="mb-1 text-sm text-at-light-green">Type</label>
+							<select
+								v-if="edit"
+								v-model="item.cardioType"
+								id="cardioType"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							>
+								<option value="#">Select Type</option>
+								<option value="run">Run</option>
+								<option value="walk">Walk</option>
+							</select>
+							<p v-else>{{ item.cardioType }}</p>
+						</div>
+						<div class="flex flex-1 flex-col">
+							<label for="distance" class="mb-1 text-sm text-at-light-green">Distance</label>
+							<input
+								v-if="edit"
+								v-model="item.distance"
+								id="distance"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							/>
+							<p v-else>{{ item.distance }}</p>
+						</div>
+						<div class="flex flex-1 flex-col">
+							<label for="duration" class="mb-1 text-sm text-at-light-green">Duration</label>
+							<input
+								v-if="edit"
+								v-model="item.duration"
+								id="duration"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							/>
+							<p v-else>{{ item.duration }}</p>
+						</div>
+						<div class="flex flex-1 flex-col">
+							<label for="pace" class="mb-1 text-sm text-at-light-green">Pace</label>
+							<input
+								v-if="edit"
+								v-model="item.pace"
+								id="pace"
+								type="text"
+								class="p-2 w-full text-gray-500 focus:outline-none"
+							/>
+							<p v-else>{{ item.pace }}</p>
+						</div>
+						<img
+							v-if="edit"
+							@click="deleteExercise(item.id)"
+							class="absolute h-4 w-auto -left-5 cursor-pointer"
+							src="@/assets/images/trash-light-green.png"
+							alt="trash icon"
+						/>
+					</div>
+					<button
+						v-if="edit"
+						@click="addExercise"
+						type="button"
+						class="
+							mt-6
+							py-2
+							px-6
+							rounded-sm
+							self-start
+							text-sm text-white
+							bg-at-light-green
+							duration-200
+							border-solid border-2 border-transparent
+							hover:border-at-light-green hover:bg-white hover:text-at-light-green
+						"
+					>
+						Add Exercise
+					</button>
+				</div>
+			</div>
+			<!-- Update Workout -->
+			<button
+				v-if="edit"
+				@click="updateWorkout"
+				type="button"
+				class="
+					mt-10
+					py-2
+					px-6
+					rounded-sm
+					self-start
+					text-sm text-white
+					bg-at-light-green
+					duration-200
+					border-solid border-2 border-transparent
+					hover:border-at-light-green hover:bg-white hover:text-at-light-green
+				"
+			>
+				Update Workout
+			</button>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref, computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { uid } from 'uid';
+//
+import { supabase } from '../supabase/init';
+import store from '../store';
+
+export default {
+	name: 'view-workout',
+	setup() {
+		// Create data / vars
+		const data = ref(null);
+		const dataLoaded = ref(false);
+		const errorMsg = ref(null);
+		const statusMsg = ref(null);
+		const route = useRoute();
+		const router = useRouter();
+		const user = computed(() => store.state.user);
+
+		// Get current Id of route
+		const currentId = route.params.workoutId;
+
+		// Get workout data
+		const getData = async () => {
+			try {
+				const { data: workouts, error } = await supabase.from('workouts').select('*').eq('id', currentId);
+
+				if (error) throw error;
+
+				data.value = workouts[0];
+				dataLoaded.value = true;
+			} catch (error) {
+				errorMsg.value = error.message;
+				setTimeout(() => {
+					errorMsg.value = null;
+				}, 5000);
+			}
+		};
+
+		getData();
+
+		// Delete workout
+		const deleteWorkout = async () => {
+			try {
+				const { error } = await supabase.from('workouts').delete().eq('id', currentId);
+
+				if (error) throw error;
+
+				router.push({ name: 'Home' });
+			} catch (error) {
+				errorMsg.value = `Error: ${error.message}`;
+				setTimeout(() => {
+					errorMsg.value = null;
+				}, 5000);
+			}
+		};
+
+		// Edit mode
+		const edit = ref(false);
+
+		const editMode = () => {
+			edit.value = !edit.value;
+		};
+
+		// Add exercise
+		const addExercise = () => {
+			if (data.value.workoutType === 'strength') {
+				data.value.exercises.push({
+					id: uid(),
+					exercise: '',
+					sets: '',
+					reps: '',
+					weight: '',
+				});
+				return;
+			}
+
+			data.value.exercises.push({
+				id: uid(),
+				cardioType: '',
+				distance: '',
+				duration: '',
+				pace: '',
+			});
+		};
+
+		// Delete exercise
+		const deleteExercise = (id) => {
+			if (data.value.exercises.length > 1) {
+				data.value.exercises = data.value.exercises.filter((exercise) => exercise.id !== id);
+				return;
+			}
+			errorMsg.value = 'Error: Cannot remove, need to at least have one exercise';
+			setTimeout(() => {
+				errorMsg.value = null;
+			}, 5000);
+		};
+
+		// Update Workout
+		const updateWorkout = async () => {
+			try {
+				const { error } = await supabase
+					.from('workouts')
+					.update({
+						workoutName: data.value.workoutName,
+						exercises: data.value.exercises,
+					})
+					.eq('id', currentId);
+
+				if (error) throw error;
+
+				edit.value = false;
+				statusMsg.value = 'Success: Workout Updated';
+				setTimeout(() => {
+					statusMsg.value = null;
+				}, 5000);
+			} catch (error) {
+				errorMsg.value = `Error: ${error.message}`;
+				setTimeout(() => {
+					errorMsg.value = null;
+				}, 5000);
+			}
+		};
+
+		return {
+			data,
+			dataLoaded,
+			errorMsg,
+			statusMsg,
+			edit,
+			editMode,
+			user,
+			deleteWorkout,
+			addExercise,
+			deleteExercise,
+			updateWorkout,
+		};
+	},
+};
+</script>
